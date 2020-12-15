867a9887d95d92d1f1da3ef54c9d805da37312f4
Marjorievanderborght <web2020.marjorie@gmail.com>
2020-12-15 16:19:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<!--Nous ferons apparaître un nouveau fact sur les chats chaque fois que nous cliquerons sur l'image du chat-->
+<head>
+  <meta charset="utf-8">
+  <title>Création d'un FACT API sur les chats</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/app.js"></script>
+</head>
+<body>
+<img id="logo" src="img/cat.png">
+<p id="RandomCat">Random Cat</p>
+<div id="data">
+</body>
+</html>
